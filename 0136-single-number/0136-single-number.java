aa0324013c2448da97f5078a1c6131c6f3c99459
Insha Ali <aliinsha87@gmail.com>
2026-02-17 00:38:04 +0530

    Time: 14 ms (14.81%), Space: 47.2 MB (26.94%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+    HashMap<Integer,Integer> num=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int key=nums[i];
+            num.put(key,num.getOrDefault(key,0)+1);
+            }
+        for(int key:num.keySet()){
+            if(num.get(key)==1)
+                return key;
+        }
+      return -1;
+        }
+       
+    }
