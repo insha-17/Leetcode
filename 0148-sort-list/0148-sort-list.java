a17e93d056734538945020647cb880b4035866f3
Insha Ali <aliinsha87@gmail.com>
2026-03-06 16:50:51 +0530

    Time: 9 ms (99.06%), Space: 59.3 MB (72.46%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,27 +9,39 @@
  * }
  */
 class Solution {
+    
     public ListNode sortList(ListNode head) {
-      ListNode dummy=new ListNode(0);
-      dummy.next=head;  
-      ListNode temp=head;
-      int cnt=0;
-      while(temp!=null){
-        cnt++;
-        temp=temp.next;
+      ListNode slow=head;
+      ListNode fast=head;
+     //step1.split the LL from middle
+      if(head==null || head.next==null) return head;
+      ListNode prev=slow;
+      while(fast!=null && fast.next!=null){
+         prev=slow;
+        slow=slow.next;
+        fast=fast.next.next;
       }
-      int []arr = new int[cnt];
-      temp=head;
-      for(int i=0;i<cnt;i++){
-        arr[i]=temp.val;
-        temp=temp.next;
-      }
-      Arrays.sort(arr);
-      temp=dummy;
-      for(int j=0;j<cnt;j++){
-        temp.next.val=arr[j];
-        temp=temp.next;
-      }
-      return dummy.next;
+      prev.next=null;
+      //recursively sort the List
+      ListNode l1= sortList(head);
+      ListNode l2=sortList(slow);
+       return merge(l1,l2);
+    }
+    //helper method to merge the sorted lists
+    public ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0),result=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                result.next=l1;
+                l1=l1.next;
+            }else{
+                result.next=l2;
+                l2=l2.next;
+            }
+            result=result.next;
+        }
+        if(l1!=null)result.next=l1;
+        if(l2!=null)result.next=l2;
+        return dummy.next;
     }
 }
