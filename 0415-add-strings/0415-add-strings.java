c28e336db2c55620b7ff15c422de628ed26de75a
Insha Ali <aliinsha87@gmail.com>
2026-03-21 22:25:33 +0530

    Time: 7 ms (20.86%), Space: 47.2 MB (7.11%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        String ans="";
+     int i=num1.length()-1;
+     int j=num2.length()-1;
+     int carry=0;
+     while(i>=0 || j>=0 ||carry>0){
+         int n1=(i>=0)?num1.charAt(i)-'0':0;
+         int n2=(j>=0)?num2.charAt(j)-'0':0;
+
+         int sum=n1+ n2+carry;
+         
+         ans= (sum%10) + ans;
+         carry=sum/10;
+         
+         i--;
+         j--;
+     }
+        return ans;
+    }
+}
