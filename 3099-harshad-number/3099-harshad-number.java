8924a9e389312d09cb5c89819bbe68bfe5c1b5c4
Insha Ali <aliinsha87@gmail.com>
2026-03-23 00:09:30 +0530

    Time: 0 ms (100%), Space: 42.1 MB (76.31%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+      int sum=0;
+      int n=x;
+      while(n>0){
+        int rem=n%10;
+        sum+= rem;
+        n/=10;
+      }
+      if(x%sum==0) return sum;
+      return -1;
+    }
+}
