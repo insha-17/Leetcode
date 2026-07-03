2ba339eb3930e585fce9c1805456bc632574a1e5
Insha Ali <aliinsha87@gmail.com>
2026-07-03 16:33:47 +0530

    Time: 0 ms (100%), Space: 43.2 MB (32.37%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -16,17 +16,28 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list=new ArrayList<>();
-        preOrder(root,list);
-        return list;   
-    }
-    public void preOrder(TreeNode root,List<Integer> list){
-        if(root==null){
-            return;
-        }
-        list.add(root.val);
-
         
-        preOrder(root.left,list);
-        preOrder(root.right,list);
+        if(root==null){
+            return list;
+        }
+
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+
+        while(!st.isEmpty()){
+            root=st.pop();
+            list.add(root.val);
+
+            //check if root.right exits and if it does push into the stack before left bcs in a staxk the top should be left node as it is LIFO
+            if(root.right!=null){
+                st.push(root.right);
+            }
+            //left node check
+            if(root.left!=null){
+                st.push(root.left);
+            }
+         
+        } 
+    return list;
     }
 }
